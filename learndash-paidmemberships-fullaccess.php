f0922a11b975d65c5619644bee46cc52856b1905
adrianifero <adrianifero@gmail.com>
2015-01-12 14:14:33 -0500

    V.0.0.2
    - Preserving the courses that were already paid by a specific user
    - Ability to give access to courses belonging to a category and not to
    all courses

--- learndash-paidmemberships-fullaccess.php
+++ learndash-paidmemberships-fullaccess.php
@@ -7,7 +7,7 @@
 Plugin Name: LearnDash & Paid Memberships Pro Full Access Add On
 Plugin URI: http://www.platanocafe.ca
 Description: LearnDash Add On that integrates with the Paid Memberships Pro to allow access to all courses.  When an user adquires a membership he gets access to every single course regardless of the price for each course.  This plugin doesn't modify course's prices neither their settings.
-Version: 0.0.1
+Version: 0.0.2
 Author: Adrian Toro
 Author URI: http://www.platanocafe.ca
 */
@@ -18,13 +18,16 @@ Author URI: http://www.platanocafe.ca
 function ldpmp_fullaccess_pmpro_after_change_membership_level($level_id, $user_id) {
 	
 	// Get all courses:	
-	$filter = array( 'post_type' => 'sfwd-courses', 'posts_per_page' => -1, 'post_status' => 'publish');
+	$filter = array( 'post_type' => 'sfwd-courses', 'posts_per_page' => -1, 'post_status' => 'publish', 'cat' => 122 );
 	$loop = new WP_Query( $filter );
 	
 	while ( $loop->have_posts() ) : $loop->the_post();
 		$course_list[] = get_the_ID();
 	endwhile;
 	wp_reset_query(); 
+	
+	// Check if the user has access to some courses (paid courses):
+	$paid_courses = get_user_meta( $user_id, '_previously_enrolled_courses', true );
 	
 	// TODO:  Let the admin choose which courses he wants to grant access tosfwd_lms_has_access
 	
@@ -37,14 +40,25 @@ function ldpmp_fullaccess_pmpro_after_change_membership_level($level_id, $user_i
 	
 	if ( !empty($membership_level) && $membership_level->id >0 ):
 		foreach ( $course_list as $course_id ) {
-			if( !sfwd_lms_has_access($course_id, $user_id) )	 //Check if this user is already enrolled on this course.
-			ld_update_course_access($user_id, $course_id, $remove = false);	
+			if( !sfwd_lms_has_access($course_id, $user_id) )	{ 
+				//Check if this user is already enrolled on this course.
+				ld_update_course_access($user_id, $course_id, $remove = false);	
+			}elseif ( empty($paid_courses) || !is_array($paid_courses) ) {
+				// If the user already has access but our private variable "_previously_enrolled_courses" is not created preserve his access by saving the course ID into a variable:
+				$paid_courses[] = $course_id; 
+			}
 		}
+		update_user_meta( $user_id, '_previously_enrolled_courses', $paid_courses );
+		
 	elseif ( empty($membership_level) ):
 		foreach ( $course_list as $course_id ) {
-			ld_update_course_access($user_id, $course_id, $remove = true);	
+			if ( !in_array( $course_id, $paid_courses ) ) {
+				ld_update_course_access($user_id, $course_id, $remove = true);	
+			}
 		}
 	endif;
+	
+	
 		
 	
 	
